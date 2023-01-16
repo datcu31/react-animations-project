cb4b0c03303e2e616679ab03577c63ca42e9e374
Datcu31 <datcu31@gmail.com>
2023-01-16 16:10:56 +0200

    setup gh-pages

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import './Modal.css';
+
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+}
+
+const modal = (props) => {
+    return (
+        <CSSTransition
+            mountOnEnter
+            unmountOnExit
+            in={props.show}
+            timeout={animationTiming}
+            // classNames='fade-slide'
+            classNames={{
+                enter: '',
+                enterActive: 'ModalOpen',
+                exit: '',
+                exitActive: 'ModalClosed',
+            }}
+        >
+            <div className="Modal">
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+        </CSSTransition>
+
+    );
+
+
+};
+
+// {state => {
+//     const cssClasses = ['Modal',
+//         state === 'entering' ? "ModalOpen" :
+//             state === 'exiting' ? 'ModalClosed' : null];
+//     return (
+//         <div className={cssClasses.join(' ')}>
+//             <h1>A Modal</h1>
+//             <button className="Button" onClick={props.closed}>Dismiss</button>
+//         </div>
+//     );
+// }}
+
+export default modal;
